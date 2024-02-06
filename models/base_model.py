079ff39cdbc1c232aeb9bd30fac97793ade398ac
Prish20 <prish@students.uonbi.ac.ke>
2024-02-06 03:53:46 +0300

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,20 @@ that defines all common attributes/methods for other classes."""
 
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes a new BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime
+                        (value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
@@ -29,3 +38,29 @@ class BaseModel:
         """Return a string representation of the instance."""
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+
+if __name__ == '__main__':
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key,
+                                       type(my_model_json[key]),
+                                       my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
