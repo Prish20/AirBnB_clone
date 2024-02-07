a5fa65be63b92009600d43f0a31066b5f74d8082
Prish20 <prish@students.uonbi.ac.ke>
2024-02-07 17:51:43 +0300

    Update FileStorage to manage correctly serialization and deserialization of all our new classes: Place, State, City, Amenity and Revie

--- console.py
+++ console.py
@@ -3,10 +3,20 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 CLASSES = {
     "BaseModel": BaseModel,
-    "User": User
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
 }
 
 """This is the console module"""
